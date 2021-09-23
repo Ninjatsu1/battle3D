0a91308bf805ce6f3658c76e8335d0178a8598ac
Nina Mäkinen <sherlocked1223@gmail.com>
2021-09-23 16:43:05 +0300

    Added hit detection and enemy animation fixes

--- battleproto/Assets/scripts/Attack.cs
+++ battleproto/Assets/scripts/Attack.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace StarterAssets
@@ -9,11 +7,17 @@ namespace StarterAssets
         private Animator _animator;
         private StarterAssetsInputs _input;
 
+        public Transform attackPoint;
+        public float attackRange = 0.5f;
+        public LayerMask enemyLayers;
+
+        private int attackHash;
         // Start is called before the first frame update
         void Start()
         {
             _animator = GetComponent<Animator>();
             _input = GetComponent<StarterAssetsInputs>();
+            attackHash = Animator.StringToHash("Attack");
         }
 
         // Update is called once per frame
@@ -21,15 +25,35 @@ namespace StarterAssets
         {
             MeleeAttack();
         }
+
+        //Create hit detection
+        private void DetectHit()
+        {
+            Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayers);
+            foreach(Collider enemy in hitEnemies)
+            {
+                Debug.Log("hit" + enemy.name);
+            }
+        }
+        //Check if player pressed button, then animete 
         private void MeleeAttack()
         {
             if (_input.attack)
             {
+                DetectHit();
                 Debug.Log("Play attack");
-                _animator.SetTrigger("Attack");
+                _animator.SetTrigger(attackHash);
             }
             _input.attack = false;
         }
+
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+        }
     }
+    
 }
 
